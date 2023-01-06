49a8004c09c1cf809b32406058a8951143b0f865
Chi Wai, CHAN <chiwai.chan@canonical.com>
2023-01-06 13:59:04 +0800

    Create basic exporter skeleton.

---
+++ prometheus_juju_backup_all_exporter/exporter.py
@@ -0,0 +1,76 @@
+import socket
+import threading
+from logging import getLogger
+from socketserver import ThreadingMixIn
+from wsgiref.simple_server import WSGIRequestHandler, WSGIServer, make_server
+
+from prometheus_client import make_wsgi_app
+from prometheus_client.core import REGISTRY
+
+logger = getLogger(__name__)
+
+
+def _get_best_family(address, port):
+    """Automatically select address family depending on address."""
+    # Copied from
+    # https://github.com/prometheus/client_python/blob/master/prometheus_client/exposition.py#L152
+    infos = socket.getaddrinfo(address, port)
+    family, _, _, _, sockaddr = next(iter(infos))
+    return family, sockaddr[0]
+
+
+def _get_exporter_server(address, port):
+    """Return a threaded HTTP server with best address family."""
+    addr_family, addr = _get_best_family(str(address), int(port))
+
+    class ThreadedWSGIServer(ThreadingMixIn, WSGIServer):
+        """A WSGI server that handle requests in a separate thread."""
+
+        address_family = addr_family
+
+    return ThreadedWSGIServer, addr
+
+
+class SlientRequestHandler(WSGIRequestHandler):
+    """A Slient Request handler."""
+
+    def log_message(self, format, *args):
+        """Log nothing."""
+        pass
+
+
+class Exporter(object):
+    """The exporter class."""
+
+    def __init__(self, port, addr="0.0.0.0"):
+        """Initialize the exporter class.
+
+        Args:
+            port: Start the exporter at this port.
+            addr: Start the exporter at this address.
+        """
+        self.port = int(port)
+        self.app = make_wsgi_app()
+        self.server_class, self.addr = _get_exporter_server(addr, port)
+
+    def register(self, collector):
+        """Register collector to the exporter."""
+        REGISTRY.register(collector)
+
+    def run(self, daemon=False):
+        """Start the exporter server."""
+        httpd = make_server(
+            self.addr,
+            self.port,
+            self.app,
+            self.server_class,
+            handler_class=SlientRequestHandler,
+        )
+        logger.info(
+            "Started promethesus juju-backup-all exporter at {}:{}.".format(
+                self.addr, self.port
+            )
+        )
+        t = threading.Thread(target=httpd.serve_forever)
+        t.daemon = daemon
+        t.start()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+configs = {
+    "name": "prometheus-juju-backup-all-exporter",
+    "description": "collects backup results and exports them as metrics",
+    "use_scm_version": True,
+    "setup_requires": ["setuptools_scm", "pyyaml"],
+    "author": "Canonical BootStack DevOps Centres",
+    "packages": ["prometheus_juju_backup_all_exporter"],
+    "url": "https://github.com/canonical/prometheus-juju-backup-all-exporter",
+    "entry_points": {
+        "console_scripts": [
+            "prometheus-juju-backup-all-exporter=prometheus_juju_backup_all_exporter.__main__:main",
+        ]
+    },
+}
+
+with open("LICENSE") as f:
+    configs.update({"license": f.read()})
+
+with open("README.md") as f:
+    configs.update({"long_description": f.read()})
+
+
+if __name__ == "__main__":
+    setup(**configs)
